59bf650b0f1050811f82e231b497cc6f4b4f7180
Sh0ckWaveZero <ottooffple@icloud.com>
2020-10-13 16:07:40 +0700

    fixbug lib

--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,6 @@ const app = express()
 
 app.get('/', (req, res) => res.json({ hell0: 'world' }))
 
-app.listen(3333)
+app.listen(3333, () => {
+  console.log('Server start: listening at http://localhost:3333')
+})
